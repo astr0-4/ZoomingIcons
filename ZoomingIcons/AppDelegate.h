c6030124fe49b3bf15c127eb6411690390b56c84
Alex Stroulger <alexstroulger@gmail.com>
2015-07-16 10:40:37 -0400

    Initial Commit

---
+++ ZoomingIcons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZoomingIcons
+//
+//  Created by Alex on 2015-07-16.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZoomingIcons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZoomingIcons
+//
+//  Created by Alex on 2015-07-16.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
